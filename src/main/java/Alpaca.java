6518a80c215ebbd5979e58aac6d8ea8403443200
weihan-01 <weihan8500@gmail.com>
2025-08-29 02:13:08 -0400

    Level-0: Rename chatbot, greet, and exit

---
+++ src/main/java/Alpaca.java
@@ -0,0 +1,22 @@
+public class Alpaca {
+    public static void main(String[] args) {
+  
+        String logo = " ____        _        \n"
+                    + "|  _ \\ _   _| | _____ \n"
+                    + "| | | | | | | |/ / _ \\\n"
+                    + "| |_| | |_| |   <  __/\n"
+                    + "|____/ \\__,_|_|\\_\\___|\n";
+
+        // System.out.println("Hello from\n" + logo);
+
+        // Initial greeting
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Alpaca");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        // Exit message
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
+
