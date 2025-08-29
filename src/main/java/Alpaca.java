7a95a0cd7feff153db5197ccaa3cc689148f9fd8
weihan-01 <weihan8500@gmail.com>
2025-08-29 02:18:15 -0400

    Level-1: Echo user input; exit on 'bye'

--- src/main/java/Alpaca.java
+++ src/main/java/Alpaca.java
@@ -1,22 +1,37 @@
+import java.util.Scanner;
+
 public class Alpaca {
+
+    private static final String HR = "____________________________________________________________";
+
+    private static void printBox(String... lines) {
+        System.out.println(HR);
+        for (String line : lines) {
+            System.out.println(" " + line);
+        }
+        System.out.println(HR);
+    }
+
     public static void main(String[] args) {
-  
-        String logo = " ____        _        \n"
-                    + "|  _ \\ _   _| | _____ \n"
-                    + "| | | | | | | |/ / _ \\\n"
-                    + "| |_| | |_| |   <  __/\n"
-                    + "|____/ \\__,_|_|\\_\\___|\n";
+        // Greeting
+        printBox(
+            "Hello! I'm Alpaca",
+            "What can I do for you?"
+        );
 
-        // System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            if (!sc.hasNextLine()) break;          // handles EOF (e.g., Ctrl+Z/Ctrl+D)
+            String input = sc.nextLine();
 
-        // Initial greeting
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Alpaca");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+            if ("bye".equals(input)) {              // exit on exact "bye"
+                printBox("Bye. Hope to see you again soon!");
+                break;
+            }
 
-        // Exit message
-        System.out.println("Bye. Hope to see you again soon!");
+            // Echo the command
+            printBox(input);
+        }
+        sc.close();
     }
 }
-
